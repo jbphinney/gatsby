1052494718102c70a1ef6e28c7027aa1caea3db3
jbphinney <jbphinney@gmail.com>
2025-04-03 08:44:27 -0500

    adding index.js

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="home" />
+      <h4>Posts</h4>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div>
+          <p>{node.title}</p>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      ))}
+    </Layout>
+  )
+}
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+        }
+      }
+    }
+    }`
